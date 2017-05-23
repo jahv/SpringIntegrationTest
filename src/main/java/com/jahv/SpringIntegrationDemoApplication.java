cc5788198f742c1146822990ec0ff37be4d33714
Antonio Hernandez <ahernandez@nearsoft.com>
2017-05-22 20:23:41 -0500

    Project setup

---
+++ src/main/java/com/jahv/SpringIntegrationDemoApplication.java
@@ -0,0 +1,24 @@
+package com.jahv;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@Configuration
+@ImportResource("integration-context.xml")
+public class SpringIntegrationDemoApplication implements ApplicationRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegrationDemoApplication.class, args);
+	}
+
+
+	@Override
+	public void run(ApplicationArguments applicationArguments) throws Exception {
+		System.out.println("Hello World");
+	}
+}
