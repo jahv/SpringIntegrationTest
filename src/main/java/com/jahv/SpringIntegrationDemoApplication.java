606767587293a6c94fd99d1df9762d6070a670e0
Jose Antonio Hernandez <ahernandez@nearsoft.com>
2017-06-21 15:40:55 -0500

    Channels

--- src/main/java/com/jahv/SpringIntegrationDemoApplication.java
+++ src/main/java/com/jahv/SpringIntegrationDemoApplication.java
@@ -1,18 +1,25 @@
 package com.jahv;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.integration.channel.DirectChannel;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
 import org.springframework.messaging.support.MessageBuilder;
 
 @SpringBootApplication
 @Configuration
 @ImportResource("integration-context.xml")
 public class SpringIntegrationDemoApplication implements ApplicationRunner {
+
+    @Autowired
+    private DirectChannel channel;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIntegrationDemoApplication.class, args);
@@ -21,14 +28,16 @@ public class SpringIntegrationDemoApplication implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments applicationArguments) throws Exception {
-		//System.out.println("Hello World");
 
-//		Map<String, Object> headers = new HashMap();
-//		headers.put("key", "value");
-//		MessageHeaders messageHeaders = new MessageHeaders(headers);
-
-		//Using GenericMessage for creating a message
-//		Message message = new GenericMessage("Hello World", headers);
+	    //Message handler is subscribed to the channel
+        //When the channel sends messages they'll be managed by this handler
+	    channel.subscribe(new MessageHandler() {
+            @Override
+            public void handleMessage(Message<?> message) throws MessagingException {
+                System.out.println("FROM MESSAGE HANDLER");
+                new PrintService().print((Message<String>) message);
+            }
+        });
 
 		//Using MessageBuilder
 		Message<String> message = MessageBuilder
@@ -36,7 +45,6 @@ public class SpringIntegrationDemoApplication implements ApplicationRunner {
 				.setHeader("newKey", "newValue")
 				.build();
 
-		PrintService printService = new PrintService();
-		printService.print(message);
+		channel.send(message);
 	}
 }
